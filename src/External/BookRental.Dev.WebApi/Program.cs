1d5ea7a3d12889ba5d825c5716759e4b1dd23216
batuhanb <balibatuhan19@gmail.com>
2024-01-07 15:27:37 +0300

    Fixed not writing exceptions to the file

--- src/External/BookRental.Dev.WebApi/Program.cs
+++ src/External/BookRental.Dev.WebApi/Program.cs
@@ -10,9 +10,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddApiVersioning(config =>
 {
     config.DefaultApiVersion = new ApiVersion(1, 0);
@@ -35,14 +32,6 @@ builder.Host
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 var app = builder.Build();
-
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseExceptionHandler(_ => { });
 
